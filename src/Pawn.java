6cd68cf3e5374beb9694956eb8371458d0882f40
IliyanDraganov <iliyan982@gmail.com>
2021-02-02 18:41:00 +0200

    Implement Pawn movement

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,18 +1,122 @@
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
-public class Pawn extends Piece{
+public class Pawn extends Piece {
+    private int firstMove = -1;
 
     public Pawn(int x, int y, boolean color) {
         super(x, y, color);
     }
 
-    @Override //TODO
+    @Override
     public Pawn clone() {
-        return null;
+        return new Pawn(x, y, color);
     }
 
-    @Override //TODO
+    public void setFirstMove(int move) {
+        firstMove = move;
+    }
+
+    @Override
     public ArrayList<Position> generatePossibleMoves(Position currentBoard) {
-        return null;
+        ArrayList<Position> positions = new ArrayList<>();
+        if (color) {
+            if (currentBoard.getPiece(x + 1, y) == null) {
+                if (x == 6) {
+                    positions.add(currentBoard.promote(x, y, x + 1, y, new Queen(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y, new Knight(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y, new Rook(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y, new Bishop(x, y, color)));
+                } else {
+                    positions.add(currentBoard.move(x, y, x + 1, y));
+                }
+            }
+            if (currentBoard.getPiece(x + 1, y + 1) != null) {
+                if (x == 6) {
+                    positions.add(currentBoard.promote(x, y, x + 1, y + 1, new Queen(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y + 1, new Knight(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y + 1, new Rook(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y + 1, new Bishop(x, y, color)));
+                } else {
+                    positions.add(currentBoard.move(x, y, x + 1, y + 1));
+                }
+            }
+            if (currentBoard.getPiece(x + 1, y - 1) != null) {
+                if (x == 6) {
+                    positions.add(currentBoard.promote(x, y, x + 1, y - 1, new Queen(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y - 1, new Knight(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y - 1, new Rook(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x + 1, y - 1, new Bishop(x, y, color)));
+                } else {
+                    positions.add(currentBoard.move(x, y, x + 1, y - 1));
+                }
+            }
+            if (firstMove == -1 && currentBoard.getPiece(x + 1, y) == null && currentBoard.getPiece(x + 2, y) == null) {
+                positions.add(currentBoard.move(x, y, x + 2, y));
+            }
+            if (x == 4 && currentBoard.getPiece(x, y + 1) instanceof Pawn
+                    && ((Pawn) currentBoard.getPiece(x, y + 1)).firstMove + 1 == currentBoard.getMove()) {
+                positions.add(currentBoard.enPassant(x, y, x + 1, y + 1));
+            }
+            if (x == 4 && currentBoard.getPiece(x, y - 1) instanceof Pawn
+                    && ((Pawn) currentBoard.getPiece(x, y - 1)).firstMove + 1 == currentBoard.getMove()) {
+                positions.add(currentBoard.enPassant(x, y, x + 1, y - 1));
+            }
+        } else {
+            if (currentBoard.getPiece(x - 1, y) == null) {
+                if (x == 1) {
+                    positions.add(currentBoard.promote(x, y, x - 1, y, new Queen(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y, new Knight(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y, new Rook(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y, new Bishop(x, y, color)));
+                } else {
+                    positions.add(currentBoard.move(x, y, x - 1, y));
+                }
+            }
+            if (currentBoard.getPiece(x - 1, y + 1) != null) {
+                if (x == 1) {
+                    positions.add(currentBoard.promote(x, y, x - 1, y + 1, new Queen(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y + 1, new Knight(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y + 1, new Rook(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y + 1, new Bishop(x, y, color)));
+                } else {
+                    positions.add(currentBoard.move(x, y, x - 1, y + 1));
+                }
+            }
+            if (currentBoard.getPiece(x - 1, y - 1) != null) {
+                if (x == 1) {
+                    positions.add(currentBoard.promote(x, y, x - 1, y - 1, new Queen(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y - 1, new Knight(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y - 1, new Rook(x, y, color)));
+                    positions.add(currentBoard.promote(x, y, x - 1, y - 1, new Bishop(x, y, color)));
+                } else {
+                    positions.add(currentBoard.move(x, y, x - 1, y - 1));
+                }
+            }
+            if (firstMove == -1 && currentBoard.getPiece(x - 1, y) == null && currentBoard.getPiece(x - 2, y) == null) {
+                positions.add(currentBoard.move(x, y, x - 2, y));
+            }
+            if (x == 3 && currentBoard.getPiece(x, y + 1) instanceof Pawn
+                    && ((Pawn) currentBoard.getPiece(x, y + 1)).firstMove + 1 == currentBoard.getMove()) {
+                positions.add(currentBoard.enPassant(x, y, x - 1, y + 1));
+            }
+            if (x == 3 && currentBoard.getPiece(x, y - 1) instanceof Pawn
+                    && ((Pawn) currentBoard.getPiece(x, y - 1)).firstMove + 1 == currentBoard.getMove()) {
+                positions.add(currentBoard.enPassant(x, y, x - 1, y - 1));
+            }
+        }
+
+        return new ArrayList<>(
+                positions.stream().filter(position -> position.valid()).collect(Collectors.toList()));
+    }
+
+    @Override
+    public boolean canReach(int goalX, int goalY, Position currentPosition) {
+        if(color) {
+            if(goalX == x + 1 && (goalY == y + 1 || goalY == y - 1)) return true;
+        } else {
+            if(goalX == x - 1 && (goalY == y + 1 || goalY == y - 1)) return true;
+        }
+        return false;
     }
 }
--- src/Position.java
+++ src/Position.java
@@ -2,35 +2,80 @@
 public class Position {
     private Piece[][] board;
     private boolean turn;
+    private int move = 0;
 
-    public Position(boolean turn){
+    public Position(boolean turn, int move) {
         this.board = new Piece[8][8];
+        this.turn = turn;
+        this.move = move;
     }
 
-    public void placePiece(Piece piece){
+    public void placePiece(Piece piece) {
         board[piece.getX()][piece.getY()] = piece;
     }
 
-    public Position clone(){
-        Position newPosition = new Position(turn);
-        for(int i = 0; i < board[0].length; i++){
-            for(int j = 0; j < board.length; j++){
+    public Piece getPiece(int x, int y) {
+        if (Helpers.onBoard(x, y)) {
+            return board[x][y];
+        }
+        return null;
+    }
+    
+    public int getMove() {
+        return move;
+    }
+
+    public Position clone() {
+        Position newPosition = new Position(turn, move);
+        for (int i = 0; i < board[0].length; i++) {
+            for (int j = 0; j < board.length; j++) {
                 newPosition.placePiece(board[i][j].clone());
             }
         }
         return newPosition;
     }
 
-    public Position move(int oldX, int oldY, int newX, int newY){
-        if(!Helpers.onBoard(newX, newY)) return null;
+    public Position move(int oldX, int oldY, int newX, int newY) {
+        if (!Helpers.onBoard(newX, newY))
+            return null;
         Position newPosition = this.clone();
         newPosition.board[newX][newY] = newPosition.board[oldX][oldY];
+        newPosition.board[newX][newY].setCoordinates(newX, newY);
+        if(newPosition.board[newX][newY] instanceof Pawn) {            
+            ((Pawn)newPosition.board[newX][newY]).setFirstMove(move);
+        }
         newPosition.board[oldX][oldY] = null;
         newPosition.turn = !turn;
+        newPosition.move = move + 1;
         return newPosition.valid() ? newPosition : null;
     }
 
-    public boolean valid(){//TODO
+    public Position promote(int oldX, int oldY, int newX, int newY, Piece piece) {
+        if (!Helpers.onBoard(newX, newY))
+            return null;
+        Position newPosition = this.clone();
+        newPosition.board[newX][newY] = piece;
+        newPosition.board[newX][newY].setCoordinates(newX, newY);
+        newPosition.board[oldX][oldY] = null;
+        newPosition.turn = !turn;
+        newPosition.move = move + 1;
+        return newPosition.valid() ? newPosition : null;
+    }
+    
+    public Position enPassant(int oldX, int oldY, int newX, int newY) {
+        if (!Helpers.onBoard(newX, newY))
+            return null;
+        Position newPosition = this.clone();
+        newPosition.board[newX][newY] = newPosition.board[oldX][oldY];
+        newPosition.board[newX][newY].setCoordinates(newX, newY);
+        newPosition.board[oldX][oldY] = null;
+        newPosition.board[oldX][newY] = null;
+        newPosition.turn = !turn;
+        newPosition.move = move + 1;
+        return newPosition.valid() ? newPosition : null;
+    }
+
+    public boolean valid() {// TODO
         return true;
     }
 }
