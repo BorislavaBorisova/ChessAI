850ed613d66541e06d290d79d92a14a1d3bad631
IliyanDraganov <iliyan982@gmail.com>
2021-02-05 13:37:13 +0200

    Change Bishop and King to use Move

--- src/King.java
+++ src/King.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class King extends Piece {
     private boolean hasMoved = false;
@@ -22,8 +21,8 @@ public class King extends Piece {
     }
 
     @Override
-    public ArrayList<Position> generatePossibleMoves(Position currentBoard) {
-        ArrayList<Position> positions = new ArrayList<>();
+    public ArrayList<Move> generatePossibleMoves(Position currentBoard) {
+        ArrayList<Move> moves = new ArrayList<>();
         Piece down = currentBoard.getPiece(x, y - 1);
         Piece downRight = currentBoard.getPiece(x + 1, y - 1);
         Piece right = currentBoard.getPiece(x + 1, y);
@@ -33,33 +32,32 @@ public class King extends Piece {
         Piece left = currentBoard.getPiece(x - 1, y);
         Piece downLeft = currentBoard.getPiece(x - 1, y - 1);
         if (down == null || (down != null && down.color != color)) {
-            positions.add(currentBoard.move(x, y, x, y - 1));
+            moves.add(new Move(this.clone(), x, y - 1, currentBoard.getPiece(x, y - 1), null, null));
         }
         if (downRight == null || (downRight != null && downRight.color != color)) {
-            positions.add(currentBoard.move(x, y, x + 1, y - 1));
+            moves.add(new Move(this.clone(), x + 1, y - 1, currentBoard.getPiece(x + 1, y - 1), null, null));
         }
         if (right == null || (right != null && right.color != color)) {
-            positions.add(currentBoard.move(x, y, x + 1, y));
+            moves.add(new Move(this.clone(), x + 1, y, currentBoard.getPiece(x + 1, y), null, null));
         }
         if (upRight == null || (upRight != null && upRight.color != color)) {
-            positions.add(currentBoard.move(x, y, x + 1, y + 1));
+            moves.add(new Move(this.clone(), x + 1, y + 1, currentBoard.getPiece(x + 1, y + 1), null, null));
         }
         if (up == null || (up != null && up.color != color)) {
-            positions.add(currentBoard.move(x, y, x, y + 1));
+            moves.add(new Move(this.clone(), x, y + 1, currentBoard.getPiece(x, y + 1), null, null));
         }
         if (upLeft == null || (upLeft != null && upLeft.color != color)) {
-            positions.add(currentBoard.move(x, y, x - 1, y + 1));
+            moves.add(new Move(this.clone(), x - 1, y + 1, currentBoard.getPiece(x - 1, y + 1), null, null));
         }
         if (left == null || (left != null && left.color != color)) {
-            positions.add(currentBoard.move(x, y, x - 1, y));
+            moves.add(new Move(this.clone(), x - 1, y, currentBoard.getPiece(x - 1, y), null, null));
         }
         if (downLeft == null || (downLeft != null && downLeft.color != color)) {
-            positions.add(currentBoard.move(x, y, x - 1, y - 1));
+            moves.add(new Move(this.clone(), x - 1, y - 1, currentBoard.getPiece(x - 1, y - 1), null, null));
         }
         if (!hasMoved) {
-            if (color) {
-                Piece leftCorner = currentBoard.getPiece(0, 0);
-                Piece rightCorner = currentBoard.getPiece(7, 0);
+                Piece leftCorner = currentBoard.getPiece(0, y);
+                Piece rightCorner = currentBoard.getPiece(7, y);
                 Rook leftRook = null, rightRook = null;
                 if (leftCorner instanceof Rook) {
                     leftRook = (Rook) leftCorner;
@@ -67,42 +65,21 @@ public class King extends Piece {
                 if (rightCorner instanceof Rook) {
                     rightRook = (Rook) rightCorner;
                 }
-                if (leftRook != null && !leftRook.hasMoved() && currentBoard.getPiece(3, 0) == null
-                        && currentBoard.getPiece(2, 0) == null && currentBoard.getPiece(1, 0) == null
-                        && !currentBoard.canBeAttacked(3, 0)
-                        && !currentBoard.canBeAttacked(2, 0)) {
-                    positions.add(currentBoard.castle(x, y, 0, 0));
+                if (leftRook != null && !leftRook.hasMoved() && currentBoard.getPiece(x - 1, y) == null
+                        && currentBoard.getPiece(x - 2, y) == null && currentBoard.getPiece(x - 3, y) == null
+                        && !currentBoard.canBeAttacked(x - 1, y)
+                        && !currentBoard.canBeAttacked(x - 2, y)) {
+                    moves.add(new Move(this.clone(), x - 2, y, null, leftRook.clone(), null));
                 }
-                if (rightRook != null && !rightRook.hasMoved() && currentBoard.getPiece(5, 0) == null
-                        && currentBoard.getPiece(6, 0) == null && !currentBoard.canBeAttacked(5, 0)
-                        && !currentBoard.canBeAttacked(6, 0)) {
-                    positions.add(currentBoard.castle(x, y, 7, 0));
+                if (rightRook != null && !rightRook.hasMoved() && currentBoard.getPiece(x + 1, 0) == null
+                        && currentBoard.getPiece(x + 2, 0) == null && !currentBoard.canBeAttacked(x + 1, 0)
+                        && !currentBoard.canBeAttacked(x + 2, 0)) {
+                    moves.add(new Move(this.clone(), x + 2, y, null, rightRook.clone(), null));
                 }
-            } else {
-                Piece leftCorner = currentBoard.getPiece(0, 7);
-                Piece rightCorner = currentBoard.getPiece(7, 7);
-                Rook leftRook = null, rightRook = null;
-                if (leftCorner instanceof Rook) {
-                    leftRook = (Rook) leftCorner;
-                }
-                if (rightCorner instanceof Rook) {
-                    rightRook = (Rook) rightCorner;
-                }
-                if (leftRook != null && !leftRook.hasMoved() && currentBoard.getPiece(3, 7) == null
-                        && currentBoard.getPiece(2, 7) == null && currentBoard.getPiece(1, 7) == null
-                        && !currentBoard.canBeAttacked(3, 7)
-                        && !currentBoard.canBeAttacked(2, 7)) {
-                    positions.add(currentBoard.castle(x, y, 0, 7));
-                }
-                if (rightRook != null && !rightRook.hasMoved() && currentBoard.getPiece(5, 7) == null
-                        && currentBoard.getPiece(6, 7) == null && !currentBoard.canBeAttacked(5, 7)
-                        && !currentBoard.canBeAttacked(6, 7)) {
-                    positions.add(currentBoard.castle(x, y, 7, 7));
-                }
-            }
+            
         }
 
-        return new ArrayList<>(positions.stream().filter(position -> position != null).collect(Collectors.toList()));
+        return moves;
     }
 
     @Override
